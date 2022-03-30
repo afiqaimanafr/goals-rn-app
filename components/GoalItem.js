4a7e76ab86dcaf8032543358c547c9a63aacad6d
afiqaimanafr <aaafr98@gmail.com>
2022-03-30 09:59:28 +0800

    update: latest

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -4,7 +4,7 @@ function GoalItem(props) {
   return (
     <View style={styles.goalItem}>
       <Pressable
-        android_ripple={{ color: "#dddddd" }}
+        android_ripple={{ color: "#210644" }}
         onPress={props.onDeleteItem.bind(this, props.id)}
         style={(pressed) => pressed && styles.pressedItem}
       >
